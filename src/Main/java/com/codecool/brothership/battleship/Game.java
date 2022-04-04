205efbd68104e330eadc2c50b45ef90ea86db066
benedekhalaj <bendihalaj@gmail.com>
2022-04-04 13:55:03 +0200

    Add Game.placeShipsForPlayer

--- src/Main/java/com/codecool/brothership/battleship/Game.java
+++ src/Main/java/com/codecool/brothership/battleship/Game.java
@@ -3,17 +3,23 @@ package com.codecool.brothership.battleship;
 import com.codecool.brothership.utilities.Display;
 import com.codecool.brothership.utilities.Input;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
     private final Display display;
     private final Input input;
     private final GameMode gameMode;
     private final AiType aiType;
+    private PlayerType player1Type;
+    private PlayerType player2Type;
 
     public Game(Display display, Input input, GameMode gameMode, AiType aiType) {
         this.display = display;
         this.input = input;
         this.gameMode = gameMode;
         this.aiType = aiType;
+        setPlayerTypes();
     }
 
     public Game(Display display, Input input, GameMode gameMode) {
@@ -21,11 +27,55 @@ public class Game {
         this.input = input;
         this.gameMode = gameMode;
         this.aiType = null;
+        setPlayerTypes();
     }
 
     public void play() {
         // TODO game loop here
+        Player player1 = placeShipsForPlayer(player1Type);
+        Player player2 = placeShipsForPlayer(player2Type);
         System.out.println("playing in progress");
+    }
+
+    private Player placeShipsForPlayer(PlayerType playerType) {
+        List<Ship> ships;
+        if (playerType == PlayerType.PLAYER) {
+            ShipPlacementType placementType = input.getPlacementType();
+            if (placementType == ShipPlacementType.MANUAL) {
+                ships = placeShipsManually();
+            } else {
+                ships = placeShipsRandomly();
+            }
+        } else {
+            ships = placeShipsRandomly();
+        }
+        return new Player(ships);
+    }
+
+    private List<Ship> placeShipsManually() {
+        // TODO place ships manually in a loop
+        return new ArrayList<>();
+    }
+
+    private List<Ship> placeShipsRandomly() {
+        // TODO place ships randomly
+        return new ArrayList<>();
+    }
+
+    private void setPlayerTypes() {
+        switch (gameMode) {
+            case P_VS_P:
+                player1Type = PlayerType.PLAYER;
+                player2Type = PlayerType.PLAYER;
+                break;
+            case P_VS_AI:
+                player1Type = PlayerType.PLAYER;
+                player2Type = PlayerType.AI;
+                break;
+            case AI_VS_AI:
+                player1Type = PlayerType.AI;
+                player2Type = PlayerType.AI;
+        }
     }
 
     private void playRound() {
