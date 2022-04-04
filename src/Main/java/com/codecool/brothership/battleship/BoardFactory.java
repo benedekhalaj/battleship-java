0ab967abf01201019ac2b7da662e5692356c6204
benedekhalaj <bendihalaj@gmail.com>
2022-04-04 14:17:57 +0200

    Rework BoardFactory

--- src/Main/java/com/codecool/brothership/battleship/BoardFactory.java
+++ src/Main/java/com/codecool/brothership/battleship/BoardFactory.java
@@ -1,13 +1,21 @@
 package com.codecool.brothership.battleship;
 
+import java.util.List;
+
 public class BoardFactory {
+    private final int size;
 
-    public static Board[][] randomPlacement(int rows, int cols) {
-        return new Board[rows][cols];
+    public BoardFactory(int size) {
+        this.size = size;
     }
 
-    public static Board[][] manualPlacement(int rows, int cols) {
-        return new Board[rows][cols];
+    public Board randomPlacement() {
+        Square[][] board = new Square[size][size];
+        return new Board(board);
     }
 
+    public Board manualPlacement(List<Ship> ships) {
+        Square[][] board = new Square[size][size];
+        return new Board(board);
+    }
 }
