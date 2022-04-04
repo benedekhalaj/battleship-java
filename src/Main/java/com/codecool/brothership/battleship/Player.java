9e48956f4d0b345962b0757afb5d5dc71679aca2
dudaskobende <dudaskobende@gmail.com>
2022-04-04 07:52:55 +0200

    Setup Player class

--- src/Main/java/com/codecool/brothership/battleship/Player.java
+++ src/Main/java/com/codecool/brothership/battleship/Player.java
@@ -1,4 +1,21 @@
 package com.codecool.brothership.battleship;
 
+import java.util.List;
+
 public class Player {
+    private final List<Ship> ships;
+
+    public Player(List<Ship> ships) {
+        this.ships = ships;
+    }
+
+    public Square shoot() {
+        // TODO returns valid shot square
+        return new Square();
+    }
+
+    public boolean isAlive() {
+        // TODO return tru if player has ship, what has not sunk yet
+        return true;
+    }
 }
