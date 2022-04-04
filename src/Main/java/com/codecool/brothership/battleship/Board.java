b2937d348eb1ff7259a5d2edd3d60f855ca87c9a
benedekhalaj <bendihalaj@gmail.com>
2022-04-04 18:31:34 +0200

    Add Board.addShip

--- src/Main/java/com/codecool/brothership/battleship/Board.java
+++ src/Main/java/com/codecool/brothership/battleship/Board.java
@@ -7,6 +7,10 @@ public class Board {
         this.ocean = ocean;
     }
 
+    public void addShip(Ship ship) {
+        // TODO Implement method to add Ship to ocean from ship squares
+    }
+
     public boolean isPlaceOk(Square square) {
         return true;
     }
