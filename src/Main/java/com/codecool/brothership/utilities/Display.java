73e92708d6baf4ba9547adcc6c6529bcc009714e
dudaskobende <dudaskobende@gmail.com>
2022-04-04 08:06:17 +0200

    Setup Display class core methods

--- src/Main/java/com/codecool/brothership/utilities/Display.java
+++ src/Main/java/com/codecool/brothership/utilities/Display.java
@@ -1,4 +1,22 @@
 package com.codecool.brothership.utilities;
 
+import com.codecool.brothership.battleship.Player;
+import com.codecool.brothership.battleship.Square;
+
 public class Display {
+    public void printBoard(Square[][] board) {
+        // TODO prints the board(ocean)
+    }
+
+    public void printMenu() {
+        // TODO Prints menu
+    }
+
+    public void printGameplay(Square[][] board, Player player) {
+        // TODO prints the gameplay
+    }
+
+    public void printResult() {
+        // TODO prints the game result
+    }
 }
