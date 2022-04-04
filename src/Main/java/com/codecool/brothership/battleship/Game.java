77d141264c79d1bfa83083aa098ff12252f86dc1
dudaskobende <dudaskobende@gmail.com>
2022-04-04 07:47:21 +0200

    Setup Game class

--- src/Main/java/com/codecool/brothership/battleship/Game.java
+++ src/Main/java/com/codecool/brothership/battleship/Game.java
@@ -1,4 +1,25 @@
 package com.codecool.brothership.battleship;
 
 public class Game {
+    private final GameMode gameMode;
+    private final AiType aiType;
+
+    public Game(GameMode gameMode, AiType aiType) {
+        this.gameMode = gameMode;
+        this.aiType = aiType;
+    }
+
+    public void play() {
+        // TODO game loop here
+        System.out.println("playing in progress");
+    }
+
+    private void playRound() {
+        // TODO players make moves.
+    }
+
+    private boolean hasWon() {
+        //TODO return true if the game has ended by rules
+        return false;
+    }
 }
