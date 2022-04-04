1cf721a0a08bef5cf51064a3bce944d7520861c5
dudaskobende <dudaskobende@gmail.com>
2022-04-04 07:55:18 +0200

    Setup Ship class

--- src/Main/java/com/codecool/brothership/battleship/Ship.java
+++ src/Main/java/com/codecool/brothership/battleship/Ship.java
@@ -1,4 +1,19 @@
 package com.codecool.brothership.battleship;
 
 public class Ship {
+    private final ShipType type;
+    private final Square[] coordinates;
+
+    public Ship(ShipType type, Square[] coordinates) {
+        this.type = type;
+        this.coordinates = coordinates;
+    }
+
+    public ShipType getType() {
+        return type;
+    }
+
+    public Square[] getCoordinates() {
+        return coordinates;
+    }
 }
