36908c4f57d0d5c270654c7250b997484a922300
dudaskobende <dudaskobende@gmail.com>
2022-04-04 07:30:48 +0200

    Setup Board class

--- src/Main/java/com/codecool/brothership/battleship/Board.java
+++ src/Main/java/com/codecool/brothership/battleship/Board.java
@@ -1,4 +1,13 @@
 package com.codecool.brothership.battleship;
 
 public class Board {
+    private final Square[][] ocean;
+
+    public Board(int rows, int cols) {
+        this.ocean = new Square[rows][cols];
+    }
+
+    public boolean isPlaceOk(Square square) {
+        return true;
+    }
 }
