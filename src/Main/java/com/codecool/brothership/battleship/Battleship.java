cf48ac6b9aca13bed57c36ac7dffb49db3778609
dudaskobende <dudaskobende@gmail.com>
2022-04-04 07:25:45 +0200

    Setup Battleship class core fields

--- src/Main/java/com/codecool/brothership/battleship/Battleship.java
+++ src/Main/java/com/codecool/brothership/battleship/Battleship.java
@@ -1,4 +1,9 @@
 package com.codecool.brothership.battleship;
 
+import com.codecool.brothership.utilities.Display;
+import com.codecool.brothership.utilities.Input;
+
 public class Battleship {
+    private final Display display = new Display();
+    private final Input input = new Input();
 }
