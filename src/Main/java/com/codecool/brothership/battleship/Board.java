71ba7c795bde45899e043970a681b5c1d91904e2
benedekhalaj <bendihalaj@gmail.com>
2022-04-05 10:39:10 +0200

    Add Board constructor to fill board with EMPTY squares

--- src/Main/java/com/codecool/brothership/battleship/Board.java
+++ src/Main/java/com/codecool/brothership/battleship/Board.java
@@ -5,7 +5,12 @@ public class Board {
 
     public Board(int size) {
         // TODO Implement filling board
-        this.ocean = new Square[size][size];
+        ocean = new Square[size][size];
+        for (int y = 0; y < ocean.length; y++) {
+            for (int x = 0; x < ocean[y].length; x++) {
+                ocean[y][x] = new Square(x, y);
+            }
+        }
     }
 
     public void addShip(Ship ship) {
