8e53a730036c0eb6d376753786201986e11e0961
dudaskobende <dudaskobende@gmail.com>
2022-04-06 14:07:08 +0200

    Setup RandomHelper class

---
+++ src/Main/java/com/codecool/brothership/utilities/RandomHelper.java
@@ -0,0 +1,11 @@
+package com.codecool.brothership.utilities;
+
+import java.util.Random;
+
+public class RandomHelper {
+    Random random = new Random();
+
+    public Random getRandom() {
+        return random;
+    }
+}
