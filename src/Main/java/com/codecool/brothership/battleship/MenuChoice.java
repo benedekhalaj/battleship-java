f0f44592f08b2865b508b94d8b7c65c5f97c33fe
benedekhalaj <bendihalaj@gmail.com>
2022-04-06 11:24:45 +0200

    Add MenuChoice.getMenuChoiceByUserInput method

--- src/Main/java/com/codecool/brothership/battleship/MenuChoice.java
+++ src/Main/java/com/codecool/brothership/battleship/MenuChoice.java
@@ -13,4 +13,13 @@ public enum MenuChoice {
     public String getInputId() {
         return inputId;
     }
+
+    public static MenuChoice getMenuChoiceByUserInput(String userInput) {
+        for (MenuChoice menuChoice : values()) {
+            if (menuChoice.inputId.equals(userInput)) {
+                return menuChoice;
+            }
+        }
+        return null;
+    }
 }
