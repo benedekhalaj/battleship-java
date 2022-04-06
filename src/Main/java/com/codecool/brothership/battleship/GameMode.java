e963e5e8c350d1980538aa8c85a9891da48e118f
benedekhalaj <bendihalaj@gmail.com>
2022-04-06 09:05:48 +0200

    Add GameMode.inputId field

--- src/Main/java/com/codecool/brothership/battleship/GameMode.java
+++ src/Main/java/com/codecool/brothership/battleship/GameMode.java
@@ -1,7 +1,17 @@
 package com.codecool.brothership.battleship;
 
 public enum GameMode {
-    P_VS_P,
-    P_VS_AI,
-    AI_VS_AI;
+    P_VS_P("1"),
+    P_VS_AI("2"),
+    AI_VS_AI("3");
+
+    private final String inputId;
+
+    GameMode(String inputId) {
+        this.inputId = inputId;
+    }
+
+    public String getInputId() {
+        return inputId;
+    }
 }
